1a9d5502ad97cac4cbf2e4f232221d86654db47a
Lucas Lins <lucas.fsl.92@gmail.com>
2024-02-26 11:00:04 -0300

    feature: ajustando pastas

--- src/app/services/import.service.ts
+++ src/app/services/import.service.ts
@@ -3,7 +3,7 @@ import make_card from "../../dtos/make_card.dto";
 class ImportService{
 
     public async import(){
-        let jsonFile = require('../../public/storage/sulamericaodontolooping_1.json');
+        let jsonFile = require('../../../public/storage/sulamericaodontolooping_1.json');
         const objs = Object.values(jsonFile.flow);
         objs.forEach(function(obj:any){
            make_card(obj); 
